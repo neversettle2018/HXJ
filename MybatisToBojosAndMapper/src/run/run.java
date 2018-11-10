fa3ccda43914b67d7bb81d81640d34b9298ae270
neversettle2018 <1057148049@qq.com>
2018-11-10 16:43:00 +0800

    mybatis逆向工程生成pojo和mapper
    mybatis逆向工程生成pojo和mapper

---
+++ MybatisToBojosAndMapper/src/run/run.java
@@ -0,0 +1,52 @@
+package run;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+public class run {
+
+	public void generator() throws Exception{
+
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		//指定 逆向工程配置文件
+		File configFile = new File("generatorConfig.xml"); 
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configFile);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+				callback, warnings);
+		myBatisGenerator.generate(null);
+
+	} 
+	public static void main(String[] args) throws Exception {
+		try {
+			run generatorSqlmap = new run();
+			generatorSqlmap.generator();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	/*测试方法 得到spring容器进行测试
+	private ApplicationContext applicationContext;
+	
+	private ItemsMapper itemsMapper;
+
+	//在setUp这个方法得到spring容器
+	@Before
+	public void setUp() throws Exception {
+		applicationContext = new ClassPathXmlApplicationContext("classpath:spring/applicationContext.xml");
+		itemsMapper = (ItemsMapper) applicationContext.getBean("itemsMapper");
+	}
+    */
+}
